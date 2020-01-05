c88982bb0a0497b08d7c4a4e5572cd5322deed62
Sergii <serenya@live.com>
2020-01-05 21:30:01 +0100

    Add tests

---
+++ src/tests.py
@@ -0,0 +1,16 @@
+import unittest
+import server as tested_app
+
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')
+        self.assertEqual(r.data, b'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
